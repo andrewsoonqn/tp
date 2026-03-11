2d3d504570fa1eae4f023afd497ece8648fa0045
hazel <167740146+hqzell@users.noreply.github.com>
2026-03-11 19:48:14 +0800

    Change Address field to Room field (#80)
    * Change Address field to Room field and change all instances of address to room
    
    * Update to room in PersonListCard.fxml and PersonBuilder.java and fix lexicographical order of imports
    
    * Format changes to pass check style
    
    * Change address to room in all tests
    
    * Change occurences of address to room
    
    ---------
    
    Co-authored-by: Andrew Soon <andrewsoonqn@gmail.com>

---
+++ src/test/java/seedu/address/model/person/RoomTest.java
@@ -0,0 +1,56 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class RoomTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Room(null));
+    }
+
+    @Test
+    public void constructor_invalidRoom_throwsIllegalArgumentException() {
+        String invalidRoom = "";
+        assertThrows(IllegalArgumentException.class, () -> new Room(invalidRoom));
+    }
+
+    @Test
+    public void isValidRoom() {
+        // null room
+        assertThrows(NullPointerException.class, () -> Room.isValidRoom(null));
+
+        // invalid rooms
+        assertFalse(Room.isValidRoom("")); // empty string
+        assertFalse(Room.isValidRoom(" ")); // spaces only
+
+        // valid rooms
+        assertTrue(Room.isValidRoom("Blk 456, Den Road, #01-355"));
+        assertTrue(Room.isValidRoom("-")); // one character
+        assertTrue(Room.isValidRoom("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long room
+    }
+
+    @Test
+    public void equals() {
+        Room room = new Room("Valid Room");
+
+        // same values -> returns true
+        assertTrue(room.equals(new Room("Valid Room")));
+
+        // same object -> returns true
+        assertTrue(room.equals(room));
+
+        // null -> returns false
+        assertFalse(room.equals(null));
+
+        // different types -> returns false
+        assertFalse(room.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(room.equals(new Room("Other Valid Room")));
+    }
+}
