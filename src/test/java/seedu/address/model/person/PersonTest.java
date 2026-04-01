15a8d5b57125ac16c6b99177956cd54d87e287b8
Andrew <159703254+andrewsoonqn@users.noreply.github.com>
2026-04-01 22:37:07 +0800

    Update identity test to include remark
    Add .withComment(VALID_COMMENT_BOB) to the isSamePerson test case that
    changes all other attributes to verify isSamePerson correctly ignores
    remarks when comparing persons by name.

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_COMMENT_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_COMMENT_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
@@ -35,7 +36,7 @@ public class PersonTest {
 
         // same name, all other attributes different -> returns true
         Person editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
-                .withRoom(VALID_ROOM_BOB).withTags(VALID_TAG_ALLERGIES).build();
+                .withRoom(VALID_ROOM_BOB).withComment(VALID_COMMENT_BOB).withTags(VALID_TAG_ALLERGIES).build();
         assertTrue(ALICE.isSamePerson(editedAlice));
 
         // different name, all other attributes same -> returns false
